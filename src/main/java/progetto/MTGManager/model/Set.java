fdb124cea6b580be4871dc83aa18069cb38cf78e
Osareph <manumanfro95@gmail.com>
2019-06-05 18:12:51 +0200

    added class Set

--- src/main/java/progetto/MTGManager/model/Set.java
+++ src/main/java/progetto/MTGManager/model/Set.java
@@ -1,22 +1,16 @@
 package progetto.MTGManager.model;
 
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Collezione {
-	
+public class Set {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
 	private String nome;
-	
-	private Utente utente;
-	
-	private List<Carta> cartePossedute;
 }
