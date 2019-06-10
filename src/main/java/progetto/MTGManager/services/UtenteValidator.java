740d9ccf9bc3c164858608a6915cc979fa7c3d70
Osareph <manumanfro95@gmail.com>
2019-06-10 11:34:10 +0200

    Update validator and singUp.html
    ho aggiunto al validetor la'istanza per l'username, ed ho cambiato il nome della form in "registrazione"

--- src/main/java/progetto/MTGManager/services/UtenteValidator.java
+++ src/main/java/progetto/MTGManager/services/UtenteValidator.java
@@ -20,7 +20,8 @@ public class UtenteValidator implements Validator {
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "nome", "required");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "cognome", "required");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "required");
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "passwordS", "required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "passwords", "required");
 	}
 
 }
