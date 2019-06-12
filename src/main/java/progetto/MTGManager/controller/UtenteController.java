29e6f6e1ac04bcab5df9778d84532985a4e00eaf
Osareph <manumanfro95@gmail.com>
2019-06-12 10:55:39 +0200

    update logIn
    ho aggiunto la vista logIn e fatto la conferma registrazione

--- src/main/java/progetto/MTGManager/controller/UtenteController.java
+++ src/main/java/progetto/MTGManager/controller/UtenteController.java
@@ -25,9 +25,6 @@ public class UtenteController {
 	@Autowired
 	private UtenteValidator utenteValidator;
 	
-	@Autowired
-	private CartaService cartaService;
-	
 	@RequestMapping(value = "/registrazione", method = RequestMethod.POST)
 	public String registrazioneUtente(@Valid @ModelAttribute("utente") Utente utente, Model model, BindingResult bindingResult) {
 		
@@ -41,25 +38,21 @@ public class UtenteController {
 		}
 	}
 	
-	@RequestMapping(value = "/collezione/{id}", method = RequestMethod.GET)
-	public String getCarta(@PathVariable ("id") Long id, Model model) {
-		if(id!=null) {
-			model.addAttribute("carta", this.cartaService.cartaPerId(id));
-			return "carta";
-		}else {
-			model.addAttribute("carte", this.cartaService.tutti());
-			return "collezione";
-		}
-	}
-	
 	@RequestMapping(value = "/singUp")
 	public String singUp(Model model) {
 		model.addAttribute("utente", new Utente());
-		return "singUp";
+		return "singUp.html";
 	}
 	
 	@RequestMapping(value = "/index")
 	public String index() {
 		return "index";
 	}
+	
+	@RequestMapping(value = "/logIn")
+	public String logIn(Model model) {
+		return "logIn";
+	}
+	
+
 }
