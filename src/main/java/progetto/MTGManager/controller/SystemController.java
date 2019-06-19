a80d686cf11f1e402989e2b05d50b2e2bea9ff2d
Osareph <manumanfro95@gmail.com>
2019-06-19 18:31:01 +0200

    update

--- src/main/java/progetto/MTGManager/controller/SystemController.java
+++ src/main/java/progetto/MTGManager/controller/SystemController.java
@@ -1,8 +1,12 @@
 package progetto.MTGManager.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import progetto.MTGManager.model.Utente;
 
--- src/main/java/progetto/MTGManager/controller/UtenteController.java
+++ src/main/java/progetto/MTGManager/controller/UtenteController.java
@@ -65,5 +65,4 @@ public class UtenteController {
 		}
 		return nextPage;
 	}
-
 }
