d78c7f5577c16c0f845c44143fc0d40fe22b9eff
Osareph <manumanfro95@gmail.com>
2019-06-19 17:09:50 +0200

    update logout

--- src/main/java/progetto/MTGManager/security/AuthConfiguration.java
+++ src/main/java/progetto/MTGManager/security/AuthConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -39,8 +40,7 @@ public class AuthConfiguration extends WebSecurityConfigurerAdapter {
 			.and()
 			
 			.logout()
-				.logoutUrl("/logOut")
-				.logoutSuccessUrl("/");
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/index");
 	}
 	
     @Bean
