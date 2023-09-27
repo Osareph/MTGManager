af551c4dca14623275830bdfcda0dac73592f432
Manuel Manfredi <11176914+Osareph@users.noreply.github.com>
2023-09-27 22:16:29 +0200

    updated
    Updated some classes

--- src/main/java/progetto/MTGManager/model/Carta.java
+++ src/main/java/progetto/MTGManager/model/Carta.java
@@ -1,14 +1,11 @@
 package progetto.MTGManager.model;
 
 
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
 
@@ -86,8 +83,4 @@ public class Carta {
 	public void setUtente(Utente utente) {
 		this.utente = utente;
 	}
-
-	
-	
-	
 }
